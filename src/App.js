dc80434cba0dacc40f8b0f6b8c5b4608e4e07dba
Nilesh <nilesh@topsinfosolutions.com>
2019-05-19 21:07:56 +0530

    Added new folder structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Login from './Components/Login/Login';
+import './App.css';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="wrapper">
+        <Login></Login>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/OldApp/components/Utility/Loader/Loader.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import './Loader.css';
+
+export default class Loader extends Component {
+
+	render() {
+		return (
+
+			<div className="loader" id="loader-6">
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+		);
+	}
+}
